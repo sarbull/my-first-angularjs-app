cb147fcd73edab0149f37db3cde7bf7d84f94b65
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2015-07-17 22:08:09 +0300

    added comments functionality

--- js/app.js
+++ js/app.js
@@ -5,18 +5,39 @@
     this.posts = posts;
   });
 
+  app.controller('PostCommentsController', function() {
+    this.comment = {};
+    this.addComment = function(product) {
+      product.comments.push(this.comment);
+      this.comment = {};
+    }
+  });
+
   var posts = [
     {
       title: 'Example post title',
       category: 'blog',
       tags: ['intro', 'example', 'cool-stuff'],
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet incidunt voluptatem totam, ducimus provident aperiam itaque iusto commodi tempora, necessitatibus, omnis id nostrum deleniti minus, esse consectetur quia magnam excepturi!'
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet incidunt voluptatem totam, ducimus provident aperiam itaque iusto commodi tempora, necessitatibus, omnis id nostrum deleniti minus, esse consectetur quia magnam excepturi!',
+      comments: [
+        {
+          stars: 2,
+          body: 'This is kinda cool',
+          author: 'John Branch'
+        },
+        {
+          stars: 2,
+          body: 'I approve your message',
+          author: 'Delta Dun'
+        }
+      ]
     },
     {
       title: 'Music is art',
       category: 'music',
       tags: ['acdc', 'pantera', 'parazitii'],
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, autem!'
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, autem!',
+      comments: []
     }
   ];
 })();
