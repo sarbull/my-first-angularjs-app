2e9b786f89a897ac937486bd73e46c90f13cf823
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2015-07-20 21:05:34 +0300

    mongo sub document prototype rest

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -7,6 +7,20 @@ var Post = require('../models/post');
 
 // Routes
 Post.methods(['get', 'put', 'post', 'delete']);
+
+router.get('/posts/:post_id/comments', function(req, res, next) {
+  Post.findById(req.params.post_id, function (err, p) {
+    res.send(p.comments);
+  });
+});
+
+router.get('/posts/:post_id/comments/:comment_id', function(req, res, next) {
+  Post.findById(req.params.post_id, function (err, p) {
+    res.send(p.comments.id(req.params.comment_id));
+  });
+});
+
+
 Post.register(router, '/posts');
 
 // Return router
